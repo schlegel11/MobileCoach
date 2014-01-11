8bd7ae964d66511534e4bb8e53423adc4571ca63
Andreas Filler <andreas@filler.name>
2014-01-11 18:41:19 +0100

    localization

---
+++ src/org/isgf/mhc/Messages.java
@@ -0,0 +1,53 @@
+package org.isgf.mhc;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import lombok.extern.log4j.Log4j2;
+
+import org.isgf.mhc.tools.UTF8Control;
+
+/**
+ * Enables localization of the whole application
+ * 
+ * @author Andreas Filler
+ */
+@Log4j2
+public class Messages {
+	private static final String		BUNDLE_NAME		= "org.isgf.mhc.messages";	//$NON-NLS-1$
+
+	private static ResourceBundle	RESOURCE_BUNDLE	= null;
+
+	/**
+	 * Set {@link Messages} class to a specific locale or fallback to default
+	 * (English)
+	 */
+	public static void setLocale(final Locale locale) {
+		try {
+			RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale,
+					new UTF8Control());
+			Locale.setDefault(locale);
+			log.debug("Set locale to {}", locale);
+		} catch (final Exception e) {
+			RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME,
+					new UTF8Control());
+			Locale.setDefault(Locale.ENGLISH);
+			log.debug("Set locale to {}", Locale.ENGLISH);
+		}
+	}
+
+	/**
+	 * Return {@link String} in currently set locale
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static String getString(final String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (final MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
