806140fd4c060c2946dfeaab023a78a44ff117e1
Andreas Filler <andreas@filler.name>
2014-02-03 17:57:19 +0100

    about window

--- src/org/isgf/mhc/ui/views/LoginView.java
+++ src/org/isgf/mhc/ui/views/LoginView.java
@@ -1,15 +1,19 @@
 package org.isgf.mhc.ui.views;
 
+import lombok.val;
 import lombok.extern.log4j.Log4j2;
 
 import org.isgf.mhc.conf.AdminMessageStrings;
+import org.isgf.mhc.conf.Messages;
 import org.isgf.mhc.ui.AdminNavigatorUI;
+import org.isgf.mhc.ui.views.components.AboutWindowComponent;
 import org.isgf.mhc.ui.views.components.LoginViewComponent;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Window;
 
 /**
  * Provides login view and navigation in login view
@@ -31,6 +35,8 @@ public class LoginView extends AbstractView implements View {
 		loginViewComponent = new LoginViewComponent();
 		loginViewComponent.getLoginButton().addClickListener(
 				new LoginButtonListener());
+		loginViewComponent.getAboutButton().addClickListener(
+				new AboutButtonListener());
 
 		// Add view
 		this.addComponent(loginViewComponent);
@@ -53,6 +59,21 @@ public class LoginView extends AbstractView implements View {
 			getAdminUI().login(
 					loginViewComponent.getUsernameField().getValue(),
 					loginViewComponent.getPasswordField().getValue());
+		}
+	}
+
+	private class AboutButtonListener implements ClickListener {
+		@Override
+		public void buttonClick(final ClickEvent event) {
+			val aboutWindow = new Window(
+					Messages.getAdminString(AdminMessageStrings.ABOUT_WINDOW__TITLE));
+			aboutWindow.setModal(true);
+			aboutWindow.setWidth("600px");
+			aboutWindow.setHeight("400px");
+			aboutWindow.setContent(new AboutWindowComponent());
+
+			getAdminUI().addWindow(aboutWindow);
+
 		}
 	}
 }
