5f01b13f406815c1c9b8e9a590ca518356a02f7a
Andreas Filler <andreas@filler.name>
2015-05-24 20:06:25 +0200

    performance improvements

--- src/ch/ethz/mc/model/Indices.java
+++ src/ch/ethz/mc/model/Indices.java
@@ -21,6 +21,18 @@ import java.util.Hashtable;
 
 import lombok.val;
 import ch.ethz.mc.model.persistent.Author;
+import ch.ethz.mc.model.persistent.DialogMessage;
+import ch.ethz.mc.model.persistent.DialogOption;
+import ch.ethz.mc.model.persistent.DialogStatus;
+import ch.ethz.mc.model.persistent.FeedbackSlide;
+import ch.ethz.mc.model.persistent.FeedbackSlideRule;
+import ch.ethz.mc.model.persistent.InterventionVariableWithValue;
+import ch.ethz.mc.model.persistent.MonitoringReplyRule;
+import ch.ethz.mc.model.persistent.MonitoringRule;
+import ch.ethz.mc.model.persistent.Participant;
+import ch.ethz.mc.model.persistent.ParticipantVariableWithValue;
+import ch.ethz.mc.model.persistent.ScreeningSurveySlide;
+import ch.ethz.mc.model.persistent.ScreeningSurveySlideRule;
 
 /**
  * Describes all indices that shall be created in the database
@@ -28,7 +40,25 @@ import ch.ethz.mc.model.persistent.Author;
  * @author Andreas Filler
  */
 public class Indices {
-	private static final String[]	authorIndices	= new String[] { "{'username':1}" };
+	private static final String[]	authorIndices							= new String[] { "{'username':1}" };
+	private static final String[]	dialogMessageIndices					= new String[] { "{'participant':1,'status':1,'shouldBeSentTimestamp':1}" };
+	private static final String[]	dialogOptionIndices						= new String[] { "{'participant':1,'type':1}" };
+	private static final String[]	dialogStatusIndices						= new String[] {
+			"{'participant':1,'dataForMonitoringParticipationAvailable':1,'screeningSurveyPerformed':1,'monitoringPerformed':1}",
+			"{'participant':1,'lastVisitedScreeningSurveySlideTimestamp':1}" };
+	private static final String[]	participantIndices						= new String[] { "{'intervention':1}" };
+
+	private static final String[]	participantVariableWithValuesIndices	= new String[] { "{'participant':1}" };
+	private static final String[]	interventionVariableWithValuesIndices	= new String[] { "{'intervention':1}" };
+
+	private static final String[]	monitoringRuleIndices					= new String[] { "{'intervention':1,'isSubRuleOfMonitoringRule':1}" };
+	private static final String[]	monitoringReplyRuleIndices				= new String[] { "{'isSubRuleOfMonitoringRule':1}" };
+
+	private static final String[]	screeningSurveySlideIndices				= new String[] { "{'screeningSurvey':1}" };
+	private static final String[]	screeningSurveySlideRuleIndices			= new String[] { "{'belongingScreeningSurveySlide':1}" };
+
+	private static final String[]	feedbackSlideIndices					= new String[] { "{'feedback':1}" };
+	private static final String[]	feedbackSlideRuleIndices				= new String[] { "{'belongingFeedbackSlide':1}" };
 
 	/**
 	 * Creates a hashtable containing all indices for all {@link ModelObject}
@@ -39,6 +69,25 @@ public class Indices {
 		val indices = new Hashtable<Class<? extends ModelObject>, String[]>();
 
 		indices.put(Author.class, authorIndices);
+		indices.put(DialogMessage.class, dialogMessageIndices);
+		indices.put(DialogOption.class, dialogOptionIndices);
+		indices.put(DialogStatus.class, dialogStatusIndices);
+		indices.put(Participant.class, participantIndices);
+
+		indices.put(ParticipantVariableWithValue.class,
+				participantVariableWithValuesIndices);
+		indices.put(InterventionVariableWithValue.class,
+				interventionVariableWithValuesIndices);
+
+		indices.put(MonitoringRule.class, monitoringRuleIndices);
+		indices.put(MonitoringReplyRule.class, monitoringReplyRuleIndices);
+
+		indices.put(ScreeningSurveySlide.class, screeningSurveySlideIndices);
+		indices.put(ScreeningSurveySlideRule.class,
+				screeningSurveySlideRuleIndices);
+
+		indices.put(FeedbackSlide.class, feedbackSlideIndices);
+		indices.put(FeedbackSlideRule.class, feedbackSlideRuleIndices);
 
 		return indices;
 	}
