b34e5c8071ccf8227df5ddcf191fbb5a3dcb534b
Andreas Filler <andreas@filler.name>
2016-08-04 15:09:24 +0200

    bugfix for missing vote information

--- src/ch/ethz/mc/rest/services/VotingService.java
+++ src/ch/ethz/mc/rest/services/VotingService.java
@@ -2,15 +2,15 @@ package ch.ethz.mc.rest.services;
 
 /*
  * Copyright (C) 2013-2016 MobileCoach Team at the Health-IS Lab
- *
+ * 
  * For details see README.md file in the root folder of this project.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -135,15 +135,15 @@ public class VotingService extends AbstractService {
 					votingVariable.setValue("0");
 					votingVariable.setVoted(false);
 				} else {
-					votingVariable.setValue(String.valueOf(StringUtils
-							.countMatches(votingVariable.getValue(), ",") + 1));
-
 					if (votingVariable.getValue().contains(
 							participantId.toHexString())) {
 						votingVariable.setVoted(true);
 					} else {
 						votingVariable.setVoted(false);
 					}
+
+					votingVariable.setValue(String.valueOf(StringUtils
+							.countMatches(votingVariable.getValue(), ",") + 1));
 				}
 			}
 
@@ -192,15 +192,15 @@ public class VotingService extends AbstractService {
 					votingVariable.setValue("0");
 					votingVariable.setVoted(false);
 				} else {
-					votingVariable.setValue(String.valueOf(StringUtils
-							.countMatches(votingVariable.getValue(), ",") + 1));
-
 					if (votingVariable.getValue().contains(
 							participantId.toHexString())) {
 						votingVariable.setVoted(true);
 					} else {
 						votingVariable.setVoted(false);
 					}
+
+					votingVariable.setValue(String.valueOf(StringUtils
+							.countMatches(votingVariable.getValue(), ",") + 1));
 				}
 			}
 
