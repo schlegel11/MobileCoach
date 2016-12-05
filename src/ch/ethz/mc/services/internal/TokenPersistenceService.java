9880276faa622a2b18169afba61c33e147992593
Pascal Pellmont <git@ppo2.ch>
2016-12-05 13:59:13 +0100

    treat tokens case insensitive. added comment for getOrCreate...

--- src/ch/ethz/mc/services/internal/TokenPersistenceService.java
+++ src/ch/ethz/mc/services/internal/TokenPersistenceService.java
@@ -8,26 +8,37 @@ import ch.ethz.mc.model.persistent.AppToken;
 import ch.ethz.mc.model.persistent.OneTimeToken;
 
 public class TokenPersistenceService {
-	
+
 	private DatabaseManagerService dbService;
 
 	public TokenPersistenceService(DatabaseManagerService dbService) {
 		this.dbService = dbService;
 	}
-	
+
 	public AppToken findAppTokenByToken(String token) {
 		return dbService.findOneModelObject(AppToken.class, "{token:#}", token);
 	}
-	
+
 	public AppToken createTokenForParticipant(ObjectId participantId) {
-		// TODO guarantee uniqueness and consistent write (probably have to generate tokenId containing participantId to avoid clashes/wrong lookups)
+		// TODO guarantee uniqueness and consistent write (probably have to
+		// generate tokenId containing participantId to avoid clashes/wrong
+		// lookups)
 		AppToken token = AppToken.create(participantId);
 		dbService.saveModelObject(token);
 		return token;
 	}
-	
+
+	/**
+	 * Finds the most recent OneTimeToken for a participant. If the most recent
+	 * token is about to expire in the next 24 hours then a new token will be
+	 * created (additionally)
+	 * 
+	 * @param participantId
+	 * @return the newest token String
+	 */
 	public String getOrCreateRecentOneTimeToken(ObjectId participantId) {
-		Iterable<OneTimeToken> tokens = dbService.findModelObjects(OneTimeToken.class, "{participantId:#}", participantId);
+		Iterable<OneTimeToken> tokens = dbService.findModelObjects(OneTimeToken.class, "{participantId:#}",
+				participantId);
 		OneTimeToken newest = null;
 		for (OneTimeToken token : tokens) {
 			if (newest == null) {
@@ -36,21 +47,21 @@ public class TokenPersistenceService {
 				newest = token;
 			}
 		}
-		if (newest == null || new Date().getTime()-newest.getCreatedAt().getTime()>24*3600*1000) {
-			
+		if (newest == null || new Date().getTime() - newest.getCreatedAt().getTime() > 24 * 3600 * 1000) {
+
 			newest = createOneTimeTokenForParticipant(participantId);
 		}
 		return newest.getToken();
 	}
-	
+
 	public OneTimeToken createOneTimeTokenForParticipant(ObjectId participantId) {
 		OneTimeToken oneTimeToken = OneTimeToken.create(participantId);
 		dbService.saveModelObject(oneTimeToken);
 		return oneTimeToken;
 	}
-	
+
 	public ObjectId consumeOneTimeToken(String token) {
-		OneTimeToken oneTimeToken = dbService.findOneModelObject(OneTimeToken.class, "{token:#}", token);
+		OneTimeToken oneTimeToken = dbService.findOneModelObject(OneTimeToken.class, "{token:#}", token.toUpperCase());
 		if (oneTimeToken == null) {
 			return null;
 		}
