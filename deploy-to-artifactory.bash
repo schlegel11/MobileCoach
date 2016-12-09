59d06bef1d51eb99431500448e5e6decb4e1243d
Pascal Pellmont <git@ppo2.ch>
2016-12-09 09:24:46 +0100

    added distributionManagementSection

--- deploy-to-artifactory.bash
+++ deploy-to-artifactory.bash
@@ -1,7 +1,6 @@
 if [ -z "$buildNumber" ]; then
     buildNumber=`date +%s`
 fi
-mvn install -DskipTests=true
-mvn test
+mvn install
 mvn deploy -Dusername=${ARTIFACTORY_USERNAME} -Dpassword=${ARTIFACTORY_PASSWORD} -Durl=${ARTIFACTORY_CONTEXT_URL}
 
