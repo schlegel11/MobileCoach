4c1a919d0e0865098effcb2f8f6d4c75bb9672db
Andreas Filler <andreas@filler.name>
2017-05-08 17:16:45 +0200

    New rules for working with select many variable results

--- src/ch/ethz/mc/conf/Constants.java
+++ src/ch/ethz/mc/conf/Constants.java
@@ -71,7 +71,7 @@ public class Constants {
 	 * 
 	 * CAUTION: Can NOT be defined in configuration file
 	 */
-	public static final int		DATA_MODEL_VERSION					= 7;
+	public static final int		DATA_MODEL_VERSION					= 8;
 	/**
 	 * Data model configuration collection
 	 * 
