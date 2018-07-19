f1a6ad9b8bb09e9f1956914d9549bf4897c3293a
Andreas Filler <andreas@filler.name>
2018-07-19 15:08:50 +0200

    switched off time logging

--- src/ch/ethz/mc/model/AbstractModelObjectAccessService.java
+++ src/ch/ethz/mc/model/AbstractModelObjectAccessService.java
@@ -37,7 +37,7 @@ import lombok.extern.log4j.Log4j2;
  */
 @Log4j2
 public abstract class AbstractModelObjectAccessService {
-	private final boolean							longQueryLogging			= true;
+	private final boolean							longQueryLogging			= false;
 	private final long								longQueryThresholdInMillis	= 50;
 
 	private final ConcurrentHashMap<String, byte[]>	idBasedModelObjectsCache;
