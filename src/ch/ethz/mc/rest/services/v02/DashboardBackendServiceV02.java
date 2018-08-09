74de3a0000b1bc60156dc8b564109cab308c03be
Andreas Filler <andreas@filler.name>
2018-08-09 18:15:57 +0200

    cleanup

--- src/ch/ethz/mc/rest/services/v02/DashboardBackendServiceV02.java
+++ src/ch/ethz/mc/rest/services/v02/DashboardBackendServiceV02.java
@@ -4,9 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import ch.ethz.mc.services.RESTManagerService;
 import lombok.extern.log4j.Log4j2;
@@ -45,26 +42,5 @@ public class DashboardBackendServiceV02 extends AbstractServiceV02 {
 		log.debug("Validation successful");
 
 		return true;
-	}
-
-	@GET
-	@Path("/getAssignedGroup")
-	@Produces("application/json")
-	public String getAssignedGroup(@HeaderParam("user") final String user,
-			@HeaderParam("password") final String password,
-			@HeaderParam("interventionPattern") final String interventionPattern) {
-		log.debug("Evaluating group for user {} with intervention pattern {}",
-				user, interventionPattern);
-
-		try {
-			return "";
-			// return restManagerService.dashboardGetAssignedGroup(user,
-			// password,
-			// interventionPattern);
-		} catch (final Exception e) {
-			throw new WebApplicationException(Response.status(Status.FORBIDDEN)
-					.entity("Could not retrieve variable: " + e.getMessage())
-					.build());
-		}
 	}
 }
