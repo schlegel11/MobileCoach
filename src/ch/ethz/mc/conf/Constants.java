10e7ccf9e57124582655236c36db79e139b6e210
Andreas Filler <andreas@filler.name>
2018-10-23 18:49:15 +0200

    new variables

--- src/ch/ethz/mc/conf/Constants.java
+++ src/ch/ethz/mc/conf/Constants.java
@@ -75,7 +75,7 @@ public class Constants {
 	 * 
 	 * CAUTION: Can NOT be defined in configuration file
 	 */
-	public static final int		DATA_MODEL_VERSION					= 42;
+	public static final int		DATA_MODEL_VERSION					= 43;
 	/**
 	 * Data model configuration collection
 	 * 
